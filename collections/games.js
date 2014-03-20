bebdd8112dbb64e43a5dc4b154ec860be78ce5c6
jfols <email@justinfoley.me>
2014-03-20 00:36:10 +0000

    Got a game schema

--- collections/games.js
+++ collections/games.js
@@ -1,1 +1,30 @@
-Games = 
+Games = new Meteor.Collection('games', 
+  { schema: new SimpleSchema({
+    name: {
+      type: String,
+      min: 5,
+      max: 100
+    },
+    players: {
+      type: [String],
+      max: 24,
+      optional: true
+    },
+    isActive: {
+      type: Boolean,
+      optional: true
+    },
+    prompt: {
+      type: String,
+      optional: false
+    },
+    text: {
+      type: String,
+      optional: false,
+      autoValue: function () { 
+        if (this.isInsert) {
+        return ''; 
+      }
+    }
+  }}) //SimpleSchema
+});
